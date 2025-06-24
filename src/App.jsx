1ec729a5eda04d19b8620165bab35ce6b4555f1c
Alexander L <losev.pavlovo152@gmail.com>
2025-06-24 05:40:02 +0300

    take section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import MainTitle from './components/MainTitle/MainTitle';
 import Detail from "./components/Detail/Detail.jsx";
+import Take from "./components/Take/Take.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Hero />
         <MainTitle />
         <Detail />
+        <Take />
       </main>
     </>
   );
---
+++ src/components/Take/Take.jsx
@@ -0,0 +1,37 @@
+import styles from './Take.module.scss';
+import classNames from "classnames";
+
+import img1 from '../../../public/images/take-1.png';
+import img2 from '../../../public/images/take-2.png';
+import img3 from '../../../public/images/take-3.png';
+
+
+import img from '../../../public/images/car-take.jpg';
+import Button from "../Button/Button.jsx";
+
+const Take = () => {
+    const Takes = [
+        {img: img1 ,title: 'Precise work',decr : 'We uphold the highest standards of professionalism when servicing your vehicles.'},
+        {img: img2 ,title: 'Premium Products and Services',decr : 'Ensure your car\'s longevity with a periodic exterior protection treatment.'},
+        {img: img3 ,title: 'High-Level Security and Privacy',decr : 'We understand the importance of privacy and security for their our clientele.'},
+    ];
+
+
+    return <section className={classNames(styles.take,'container')}>
+        <div className={styles.text}>
+            <h2 className='h2'>We will take good care of your car</h2>
+            <img src={img} alt="img"/>
+        </div>
+        <div className={styles.wrapper}>
+            {Takes.map((item, index) => (
+                <div className={styles.box} key={index}>
+                    <h3><img src={item.img} alt={item.title} />{item.title}</h3>
+                    <p>{item.decr}</p>
+                </div>
+            ))}
+            <Button title='Get a quote now'/>
+        </div>
+    </section>
+}
+
+export default Take;
